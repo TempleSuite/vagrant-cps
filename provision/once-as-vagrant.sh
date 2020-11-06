1b5353b1125a0af19d12780cadec931e8a1821d9
rruiz-univerus <59486143+rruiz-univerus@users.noreply.github.com>
2020-11-06 14:55:28 -0800

    Update once-as-vagrant.sh
    Commenting out codeception install and automatic migrations

--- provision/once-as-vagrant.sh
+++ provision/once-as-vagrant.sh
@@ -29,9 +29,9 @@ echo "Done!"
 info "Install plugins for composer"
 composer global require "fxp/composer-asset-plugin:^1.2.0" --no-progress
 
-info "Install codeception"
-composer global require "codeception/codeception=2.0.*" "codeception/specify=*" "codeception/verify=*" --no-progress
-echo 'export PATH=/home/vagrant/.config/composer/vendor/bin:$PATH' | tee -a /home/vagrant/.profile
+#info "Install codeception"
+#composer global require "codeception/codeception=2.0.*" "codeception/specify=*" "codeception/verify=*" --no-progress
+#echo 'export PATH=/home/vagrant/.config/composer/vendor/bin:$PATH' | tee -a /home/vagrant/.profile
 
 info "Cloning CPS project from github"
 git clone -b dev https://${github_token}@github.com/TempleSuite/cps-yii2.git /var/www/html/cps
@@ -46,9 +46,9 @@ info "Init CPS yii2 project"
 chmod +x ./init
 ./init --env=Development --overwrite=n
 
-info "Apply migrations"
-php yii migrate/up <<< "yes" || true
-php yii migrate --migrationPath=@vendor/maissoftware/mm-yii2/src/migrations <<< "yes" || true
+#info "Apply migrations"
+#php yii migrate/up <<< "yes" || true
+#php yii migrate --migrationPath=@vendor/maissoftware/mm-yii2/src/migrations <<< "yes" || true
 
 info "Create bash-aliases 'cps' for vagrant user"
 echo 'alias cps="cd /var/www/html/cps"' | tee /home/vagrant/.bash_aliases
